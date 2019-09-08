d5014dadd5a5c0340e4fe0251aaa85b776a823ed
Thomas Proffen <thomasproffen@gmail.com>
2019-09-08 01:27:15 -0400

    Update helpers.js

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -11,7 +11,9 @@ var index=0;
 function setup() {
 	window.addEventListener("resize", onResize);
 	document.getElementById("ble").innerHTML=connectButton();
-	document.getElementById("editBtn").innerHTML=saveEditButtons();
+	if (window.location.protocol != "file:") {
+		document.getElementById("editBtn").innerHTML=saveEditButtons();
+	}
 
 	if(!localStorage.row1) {localStorage.row1=defaultPanel(1)};
 	if(!localStorage.row2) {localStorage.row2=defaultPanel(2)};
@@ -29,7 +31,7 @@ function setup() {
 function saveData() {
 	if (document.getElementById("chartBar_div")) { document.getElementById("chartBar_div").innerHTML="";}
 	if (document.getElementById("chartLine_div")) { document.getElementById("chartLine_div").innerHTML="";}
-	document.getElementById("editBtn").innerHTML="&nbsp";
+	document.getElementById("editBtn").innerHTML="";
 
 	var data=document.documentElement.innerHTML;
 	var fileName="MicroBitMonitor.html";
